9a4d25d6aa0305e4b968f600b54833e288fbd44c
hainet50b <20400616+hainet50b@users.noreply.github.com>
2023-04-17 07:48:01 +0900

    Windows Terminalの設定を追加した。

---
+++ windows-terminal/create-settings.ps1
@@ -0,0 +1,1 @@
+sudo New-Item -Type SymbolicLink "$HOME\AppData\Local\Microsoft\Windows Terminal\settings.json" -Value $HOME\Dotfiles\windows-terminal\settings.json
---
+++ windows-terminal/create-state.ps1
@@ -0,0 +1,1 @@
+sudo New-Item -Type SymbolicLink "$HOME\AppData\Local\Microsoft\Windows Terminal\state.json" -Value $HOME\Dotfiles\windows-terminal\state.json
